c90a0111a4151c49c6f69dcae612dce8824f90d2
Chip Bell <cbell@pbs.org>
2016-07-15 14:01:55 -0400

    Calculating offsets from a key code

--- app.js
+++ app.js
@@ -15,12 +15,25 @@ function animate() {
 
 requestAnimationFrame(animate);
 
-document.body.addEventListener('keydown', function(evt) {
-  var s = String.fromCharCode(evt.which || evt.keyCode);
-  var alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-  if(alphanumeric.indexOf(s) < 0) {
-    return;
-  }
+function lookupKeyCode(keyCode) {
+  var keyCodes = [
+    [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187],
+    [9, 81, 87, 69,82, 84, 89, 85, 73, 79, 80, 219, 221, 220],
+    [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13],
+    [16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191],
+    [32],
+  ];
 
-  console.log(s);
+  for(var i = 0; i < keyCodes.length; i++) {
+    for(var j = 0; j < keyCodes[i].length; j++) {
+      if(keyCode == keyCodes[i][j]) {
+        return [i, j]
+      }
+    }
+  }
+}
+
+document.body.addEventListener('keydown', function(evt) {
+  var offset = lookupKeyCode(evt.which || evt.keyCode);
+  console.log(offset);
 });
