1d8ddb04dd491d203687cf473183296bb2ac5a95
Chip Bell <cbell@pbs.org>
2016-07-19 13:17:34 -0400

    Moving code out

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 var PIXI = require('pixi.js');
 var Blast = require('./blast');
+var Instrument = require('./instrument');
+var Keyboard = require('./keyboard');
 
 var context = new AudioContext();
-var Instrument = require('./instrument');
 
 var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x222222});
 document.body.appendChild(renderer.view);
@@ -23,26 +24,8 @@ function animate() {
 
 requestAnimationFrame(animate);
 
-function lookupKeyCode(keyCode) {
-  var keyCodes = [
-    [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187],
-    [81, 87, 69,82, 84, 89, 85, 73, 79, 80, 219, 221, 220],
-    [65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13],
-    [16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191],
-    [32],
-  ];
-
-  for(var i = 0; i < keyCodes.length; i++) {
-    for(var j = 0; j < keyCodes[i].length; j++) {
-      if(keyCode == keyCodes[i][j]) {
-        return [i, j]
-      }
-    }
-  }
-}
-
 document.body.addEventListener('keydown', function(evt) {
-  var offset = lookupKeyCode(evt.which || evt.keyCode);
+  var offset = Keyboard.lookupKeyCode(evt.which || evt.keyCode);
   if(offset === undefined) {
     return;
   }
